2cb3343a854ebe131fcf6243302f6e75c72440b0
ItFishWeb <120263519+ItFishWeb@users.noreply.github.com>
2022-12-12 16:41:00 +0100

    Update Footer.js

--- vercel+mailchimp+googleAnalitics/src/components/Footer.js
+++ vercel+mailchimp+googleAnalitics/src/components/Footer.js
@@ -19,7 +19,7 @@ export const Footer = () => {
       <td className="tdFooter"><a href="tel:+48777777777" id="footerTdLinks"><BsTelephone/> +48 777 777 777</a></td>
     </tr>
     <tr className="trFooter">
-      <td className="tdFooter"><a href="mailto:wsbcars@gmail.com" id="footerTdLinks"><BsMailbox/> wsbcars@gmail.com</a></td>
+      <td className="tdFooter"><a href="mailto:wsbcars@gmail.com" id="footerTdLinks"><BsMailbox/> wseacars@gmail.com</a></td>
     </tr>
     <tr className="trFooter">
       <td className="tdFooter"><a href="https://www.google.pl/maps/place/Jana+Długosza+2-6,+51-162+Wrocław/@51.1316955,17.0578762,17z/data=!3m1!4b1!4m5!3m4!1s0x470fe9b79a6971fb:0x39e375003f4463c4!8m2!3d51.1316955!4d17.0578762" id="footerTdLinks"><BsHouseDoor/> Ul. Jana Jakiegoś 42a Wrocław 53-239</a></td>
